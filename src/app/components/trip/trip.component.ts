776db4d15174b71b2564a1d6ae2d5f9b2cb8d46f
Gustavo Garcia <gustavol.garciar@caribanadigital.com>
2025-12-30 18:34:07 +0000

    Visual adjustments - WIP

--- src/app/components/trip/trip.component.ts
+++ src/app/components/trip/trip.component.ts
@@ -1,19 +1,18 @@
 import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
-import { ObjectsThingsCalendarAltComponent } from "../share/objects-things-calendar-alt/objects-things-calendar-alt.component";
 import { ArrowsChevronRightSmComponent } from "../share/arrows-chevron-right-sm/arrows-chevron-right-sm.component";
 import { ButtonComponent } from '../share/button/button.component';
 import { SightingRowComponent } from '../sighting-row/sighting-row.component';
-
+import { ArrowsChevronLeftComponent } from "../share/arrows-chevron-left/arrows-chevron-left.component";
 @Component({
   selector: "app-trip",
   templateUrl: "./trip.component.html",
   styleUrls: ["./trip.component.scss"],
   standalone: true,
   imports: [
-    ObjectsThingsCalendarAltComponent,
     ArrowsChevronRightSmComponent,
+    ArrowsChevronLeftComponent,
     FormsModule,
     CommonModule,
     ButtonComponent,
